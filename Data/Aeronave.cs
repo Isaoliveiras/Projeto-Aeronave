9866f74388ba268217a6f4320a5689778bee5e89
Isabella Oliveira <Isabella_oliveiras@outlook.com>
2022-12-20 21:36:54 -0300

    first commit

---
+++ Data/Aeronave.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace voeairlinesblazorvasp.Data
+{
+    public class Aeronave
+    {
+        public string? Modelo { get; set; }
+        public string? Codigo { get; set; }
+        public string? Fabricante { get; set; }
+
+        public Aeronave(string? modelo, string? codigo, string? fabricante)
+        {
+            Modelo = modelo;
+            Codigo = codigo;
+            Fabricante = fabricante;
+        }
+
+
+    }
+}
